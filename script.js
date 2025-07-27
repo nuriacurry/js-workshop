97641697474d806923a241c38bfaffddbe39f126
Nuria Siddiqa <nuri20024@gmail.com>
2025-07-27 02:29:25 -0400

    Implemented my own version of the array.map method and tested different ways to define functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// my implementation of the forEach function
+// my implementations of some common array methods
+
+// forEach runs a function on each element, and returns nothing
 function myEach(array, callback) {
     for (let i = 0; i < array.length; i++) {
         callback(array[i]);
@@ -8,3 +10,27 @@ function myEach(array, callback) {
 // testing it
 const arr = [1, 2, 3 , 4, 5];
 myEach(arr, function(number) {console.log(number);});
+
+// map runs function on each element, collects results into NEW array and returns it
+function myMap(array, callback) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let newElement = callback(array[i]);         
+        newArray.push(newElement);
+    }
+    return newArray;
+}
+
+// test it
+let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = myMap(numbers, function(number) {
+    return number * 2; 
+});
+console.log("Doubled number version 1: ", doubledNumbers);
+
+doubledNumbers = myMap(numbers, x => x * 2);
+console.log("Doubled number version 2: ", doubledNumbers);
+doubledNumbers = myMap(numbers, (x) => {return x*2;});
+console.log("Doubled number version 3: ", doubledNumbers);
+let numbersPlusOne = myMap(numbers, function(x) {return x+1;});
+console.log("numbersPlusOne", numbersPlusOne);
