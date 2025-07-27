8aa549c1b6ade2aad04f0f509a90e32901b6c38d
Nuria Siddiqa <nuri20024@gmail.com>
2025-07-27 02:03:03 -0400

    Implemented my own version of the array method forEach

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// my implementation of the forEach function
+function myEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i]);
+    }
+}
+
+// testing it
+const arr = [1, 2, 3 , 4, 5];
+myEach(arr, function(number) {console.log(number);});
